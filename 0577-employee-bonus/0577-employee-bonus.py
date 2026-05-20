bc3c7573ddad2646fdf4d46b6ab8192a855d4dc7
CHAITANYA-2002 <72995227+CHAITANYA-2002@users.noreply.github.com>
2026-05-20 09:31:52 +0530

    Time: 406 ms (5.22%), Space: 68.4 MB (62.93%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+
+    result = (
+        employee
+        .merge(bonus, on="empId", how="left")
+    )
+
+    result = result[
+        (result["bonus"] < 1000) | (result["bonus"].isna())
+    ]
+
+    return result[["name", "bonus"]]
+
+    
